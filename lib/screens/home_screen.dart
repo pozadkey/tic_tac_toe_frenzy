93432e7f1b2c0f7c46b98938f7ef5394798d7be5
POZA <poza@POZAs-MacBook-Pro.local>
2023-10-28 01:35:31 +0100

    added two buttons to homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
+import '../components/custom_button.dart';
+import '../responsive/responsive.dart';
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text('Hello'),);
+    return Scaffold(
+      body: Responsive(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomButton(
+                onTap: () {},
+                text: 'Create Room',
+              ),
+              const SizedBox(height: 20),
+              CustomButton(
+                onTap: () {},
+                text: 'Join Room',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
